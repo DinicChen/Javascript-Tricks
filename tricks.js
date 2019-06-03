4a81e2348d0384a9810b0873e049b009d07d130e
年丰 <nianfengchen000@gmail.com>
2019-06-03 00:46:50 -0500

    Update tricks.js

--- tricks.js
+++ tricks.js
@@ -591,3 +591,11 @@ function contains(refNode, otherNode){
         return false;
     }
 }
+
+
+
+function classof(o) {
+    if (o === null) return "Null";
+    if (o === undefined) return "Undefined";
+    return Object.prototype.toString.call(o).slice(8, -1);
+}
