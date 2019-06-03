25b8c71cef4bc55562e4ae1bc12c153308c1af17
年丰 <nianfengchen000@gmail.com>
2019-06-03 16:52:11 -0500

    Update tricks.js

--- tricks.js
+++ tricks.js
@@ -609,3 +609,10 @@ var a = [1, 2, 3, 4, 5]
 var sum = a.reduce(function(x,y) { return x+y }, 0); // 数组求和
 var product = a.reduce(function(x,y) { return x*y }, 1); // 数组求积
 var max = a.reduce(function(x,y) { return (x>y)?x:y; }); // 求最大值
+
+
+//判定是否为数组
+var isArray = Function.isArray || function(o) {
+        return typeof o === "object" &&
+        Object.prototype.toString.call(o) === "[object Array]";
+    };
