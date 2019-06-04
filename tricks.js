01123357831ad289e783146d5c7178c53816ffe9
年丰 <nianfengchen000@gmail.com>
2019-06-04 02:26:45 -0500

    Update tricks.js

--- tricks.js
+++ tricks.js
@@ -701,3 +701,49 @@ if (!Function.prototype.bind) {
         };
     };
 }
+
+
+
+// 对于每个数组元素调用函数f()，并返回一个结果数组
+// 如果Array.prototype.map定义了的话，就使用这个方法
+var map = Array.prototype.map ? function(a, f) { return a.map(f); } // 如果已经存在map()方法，就直接使用它
+: function(a, f) { // 否则，自己实现一个
+        var results = [];
+        for (var i = 0, len = a.length; i < len; i++) {
+            if (i in a) results[i] = f.call(null, a[i], i, a);
+        }
+        return results;
+    };
+
+// 使用函数f()和可选的初始值将数组a减至一个值
+// 如果Array.prototype.reduce存在的话，就使用这个方法
+var reduce = Array.prototype.reduce ? function(a, f, initial) { //如果reduce()方法存在的话
+        if (arguments.length > 2)
+            return a.reduce(f, initial); // 如果传入了一个初始值
+        else return a.reduce(f); // 否则没有初始值
+    } : function(a, f, initial) { // 这个算法来自ES5规范
+        var i = 0,
+            len = a.length,
+            accumulator;
+
+        // 以特定的初始值开始，否则第一个值取自a
+        if (arguments.length > 2) accumulator = initial;
+        else { //找到数组中第一个已定义的索引
+            if (len == 0) throw TypeError();
+            while (i < len) {
+                if (i in a) {
+                    accumulator = a[i++];
+                    break;
+                } else i++;
+            }
+            if (i == len) throw TypeError();
+        }
+
+        // 对于数组中剩下的元素依次调用f()
+        while (i < len) {
+            if (i in a)
+                accumulator = f.call(undefined, accumulator, a[i], i, a);
+            i++;
+        }
+        return accumulator;
+    };
