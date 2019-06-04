e68219c4d26f45e043d8e22f903fe5df9a942092
年丰 <nianfengchen000@gmail.com>
2019-06-04 17:11:18 -0500

    Update tricks.js

--- tricks.js
+++ tricks.js
@@ -748,5 +748,48 @@ var reduce = Array.prototype.reduce ? function(a, f, initial) { //如果reduce()
         return accumulator;
     };
 
+
+
 // 实现一个工具函数将类数组对象（或对象）转换为真正的数组
+// 在后面的示例代码中用到了这个方法将arguments对象转换为真正的数组
 function array(a, n) { return Array.prototype.slice.call(a, n || 0);}
+
+// 这个函数的实参传递至左侧
+function partialLeft(f /*, ...*/ ) {
+    var args = arguments; //保存外部的实参数组
+    return function() { // 并返回这个函数
+        var a = array(args, 1); // 开始处理外部的第1个args
+        a = a.concat(array(arguments)); // 然后增加所有的内部实参
+        return f.apply(this, a); // 然后基于这个实参列表调用f()
+    };
+}
+
+// 这个函数的实参传递至右侧
+function partialRight(f /*, ...*/ ) {
+    var args = arguments; // 保存外部实参数组
+    return function() { // 返回这个函数
+        var a = array(arguments); //从内部参数开始
+        a = a.concat(array(args, 1)); //然后从外部第1个args开始添加
+        return f.apply(this, a); // 最后基于这个实参列表调用f()
+    };
+}
+
+// 这个函数的实参被用做模板
+// 实参列表中的undefined值都被填充
+function partial(f /*, ... */ ) {
+    var args = arguments; //保存外部实参数组
+    return function() {
+        var a = array(args, 1); //从外部args开始
+        var i = 0,
+            j = 0;
+
+        // 遍历args，从内部实参填充undefined值
+        for (; i < a.length; i++)
+            if (a[i] === undefined) a[i] = arguments[j++];
+
+            // 现在将剩下的内部实参都追加进去
+        a = a.concat(array(arguments, j))
+        return f.apply(this, a);
+  };
+}
+
