a2cf4a2ad30f29a0eac87f32f6e799e043293a4f
年丰 <nianfengchen000@gmail.com>
2019-06-15 00:28:05 -0500

    Update tricks.js

--- tricks.js
+++ tricks.js
@@ -1031,3 +1031,17 @@ function textContent(element, value) {
         else element.innerText = value;
     }
 }
+
+// 返回元素e的纯文本内容，递归进入其子元素
+// 该方法的效果类似于textContent属性
+function textContent(e) {
+    var child, type, s = ""; // s保存所有子节点的文本
+    for (child = e.firstChild; child != null; child = child.nextSibling) {
+        type = child.nodeType;
+        if (type === 3 || type === 4) // Text和CDATASection节点
+            s += child.nodeValue;
+        else if (type === 1) // 递归Element节点
+            s += textContent(child);
+    }
+    return s;
+}
