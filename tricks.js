3eabd5ca694dfcad2915de4d31382a391ac43ebc
年丰 <nianfengchen000@gmail.com>
2019-06-21 02:33:36 -0500

    Update tricks.js

--- tricks.js
+++ tricks.js
@@ -1128,3 +1128,25 @@ function loadStyles(url){
     var head = document.getElementsByTagName("head")[0];
     head.appendChild(link);
 }
+
+
+
+// 通用DOM contains()
+function contains(refNode, otherNode){
+    if (typeof refNode.contains == "function" &&
+            (!client.engine.webkit || client.engine.webkit >= 522)){
+        return refNode.contains(otherNode);
+    } else if (typeof refNode.compareDocumentPosition == "function"){
+        return !!(refNode.compareDocumentPosition(otherNode) & 16);
+    } else {
+        var node = otherNode.parentNode;
+        do {
+            if (node === refNode){
+                return true;
+            } else {
+                node = node.parentNode;
+            }
+        } while (node !== null);
+        return false;
+    }
+}
