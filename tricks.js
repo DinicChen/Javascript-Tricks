6284526df75f7c850b01d4262c9c30162138a25f
年丰 <nianfengchen000@gmail.com>
2019-06-24 01:34:17 -0500

    Update tricks.js

--- tricks.js
+++ tricks.js
@@ -1322,6 +1322,24 @@ var EventUtil = {
         } else { 
             return null; 
         } 
-
-    }
+    },
+    getButton: function(event){ 
+        if (document.implementation.hasFeature("MouseEvents", "2.0")){ 
+            return event.button; 
+        } else { 
+            switch(event.button){ 
+                case 0: 
+                case 1:
+		case 3: 
+                case 5: 
+                case 7: 
+                    return 0; 
+                case 2: 
+                case 6: 
+                    return 2; 
+                case 4: 
+                    return 1; 
+            } 
+        } 
+    }, 
 };
