43a3bf047db9ac5d92e6bfe9d27488271438f783
年丰 <nianfengchen000@gmail.com>
2019-06-26 18:54:41 -0500

    Update tricks.js

--- tricks.js
+++ tricks.js
@@ -1358,3 +1358,20 @@ var EventUtil = {
         } 
     }, 
 };
+
+
+
+
+
+function selectText(textbox, startIndex, stopIndex){
+    if (textbox.setSelectionRange){
+        textbox.setSelectionRange(startIndex, stopIndex);
+    } else if (textbox.createTextRange){
+        var range = textbox.createTextRange();
+        range.collapse(true);
+        range.moveStart("character", startIndex);
+        range.moveEnd("character", stopIndex - startIndex);
+        range.select();
+    }
+    textbox.focus();
+}
