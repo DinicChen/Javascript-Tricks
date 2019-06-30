9cbad600184f30732273e9726080214f603966eb
年丰 <nianfengchen000@gmail.com>
2019-06-30 03:26:50 -0500

    Update tricks.js

--- tricks.js
+++ tricks.js
@@ -1458,3 +1458,16 @@ function serialize(form){
     }
     return parts.join("&");
 }
+
+
+
+
+function log(message){
+    if (typeof console == "object"){
+        console.log(message);
+    } else if (typeof opera == "object"){
+        opera.postError(message);
+    } else if (typeof java == "object" && typeof java.lang == "object"){
+        java.lang.System.out.println(message);
+    }
+}
