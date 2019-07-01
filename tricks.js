f19a3429131442803c1b0423f1d06319d99a4c0e
年丰 <nianfengchen000@gmail.com>
2019-07-01 02:22:25 -0500

    Update tricks.js

--- tricks.js
+++ tricks.js
@@ -1500,3 +1500,26 @@ function createXHR(){
         throw new Error("No XHR object available."); 
     } 
 }
+
+
+
+function createCORSRequest(method, url){
+    var xhr = new XMLHttpRequest();
+    if ("withCredentials" in xhr){
+        xhr.open(method, url, true);
+    } else if (typeof XDomainRequest != "undefined"){
+        vxhr = new XDomainRequest();
+        xhr.open(method, url);
+    } else {
+        xhr = null;
+    }
+    return xhr;
+}
+
+var request = createCORSRequest("get", "http://www.somewhere-else.com/page/");
+if (request){
+    request.onload = function(){
+        //对request.responseText进行处理
+    };
+    request.send();
+}
