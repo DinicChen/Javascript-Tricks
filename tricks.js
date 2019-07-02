ea08ba0357e6309e21d291a00c799cec2c118d68
年丰 <nianfengchen000@gmail.com>
2019-07-02 01:07:37 -0500

    Update tricks.js

--- tricks.js
+++ tricks.js
@@ -1560,3 +1560,15 @@ var client = createStreamingClient("streaming.php", function(data){
              }, function(data){
                 alert("Done!");
              });
+
+
+
+
+function curry(fn){
+    var args = Array.prototype.slice.call(arguments, 1);
+    return function(){
+        var innerArgs = Array.prototype.slice.call(arguments);
+        var finalArgs = args.concat(innerArgs);
+        return fn.apply(null, finalArgs);
+    };
+}
