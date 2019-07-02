a0581704c62c43f974c519f323abd7d1e50899ee
年丰 <nianfengchen000@gmail.com>
2019-07-02 02:44:27 -0500

    Update tricks.js

--- tricks.js
+++ tricks.js
@@ -1580,3 +1580,20 @@ function bind(fn, context){
         return fn.apply(context, finalArgs); 
     };
 }
+
+
+
+
+
+
+
+function chunk(array, process, context){
+    setTimeout(function(){
+        var item = array.shift();
+        process.call(context, item);
+
+        if (array.length > 0){
+            setTimeout(arguments.callee, 100);
+        }
+    }, 100);
+}
